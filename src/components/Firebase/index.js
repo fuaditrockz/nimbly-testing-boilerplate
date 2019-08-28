708af062fcb5002dcfb7e2bed45ba3419a31cfe1
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2019-08-28 11:54:00 +0700

    Setup Firebase API context
    The createContext() function essentially creates two components. The FirebaseContext.Provider component is used to provide a Firebase instance once at the top-level of your React component tree, which we will do in this section; and the FirebaseContext.Consumer component is used to retrieve the Firebase instance if it is needed in the React component. For a well-encapsulated Firebase module, we'll define a index.js file in our Firebase folder that exports all necessary functionalities (Firebase class, Firebase context for Consumer and Provider components):

--- src/components/Firebase/index.js
+++ src/components/Firebase/index.js
@@ -1,11 +1,4 @@
-const FirebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
-};
-
-export default FirebaseConfig;
+import FirebaseContext from './context';
+import Firebase from './firebase';
+export default Firebase;
+export { FirebaseContext };
--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-console.log(process.env.REACT_APP_API_KEY)
-
 ReactDOM.render(<App/>,
   document.getElementById('root')
 );
